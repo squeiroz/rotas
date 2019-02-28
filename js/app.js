f79e087e9857afe66596ee1d33ac76095046a473
Sanderson Queiroz <squeiroz@protonmail.com>
2019-02-27 23:58:00 -0300

    Criacao do arquivo config

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-angular.module('App',[]);
+angular.module('App',['ngRoute']);
 
-angular.module('App').controller('mainController', function($scope){
+angular.module('App').config(function($routeProvider){
+	$routeProvider.when('/home', {
+		templateUrl: "view/home.html",
+		controller: "mainController",
+		resolve: {
+			routeName: function(){
+				return "Home";
+			}
+		}
+	});
+});
+
+angular.module('App').controller('mainController', function($scope, routeName){
 	$scope.titulo = "Meu primeiro SPA com rotas";
+	$scope.routeName = routeName;
 });
