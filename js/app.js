1a9445164a6b8eebd8a83bbf4bb8c7ad06fce4c1
Sanderson Queiroz <squeiroz@protonmail.com>
2019-02-28 01:14:53 -0300

    Correcao de variaveis na config

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
 angular.module('App',['ngRoute']);
 
 angular.module('App').config(function($routeProvider){
-	$routeProvider.when('/home', {
-		templateUrl: "view/home.html",
-		controller: "mainController",
-		resolve: {
-			routeName: function(){
-				return "Home";
-			}
-		}
-	});
+    $routeProvider.when('/home',{
+        templateUrl:"view/home.html",
+        controller:"mainController",
+        resolve:{
+            routeName:function(){
+                return "Home";
+            }
+        }
+    });
+    $routeProvider.when('/contato',{
+        templateUrl:"view/contato.html",
+        controller:"mainController",
+        resolve:{
+            routeName:function(){
+                return "Contato";
+            }
+        }
+    });
+
+    $routeProvider.otherwise({redirectTo:"/home"});
 });
 
 angular.module('App').controller('mainController', function($scope, routeName){
-	$scope.titulo = "Meu primeiro SPA com rotas";
+	$scope.titulo = "myApp";
 	$scope.routeName = routeName;
 });
